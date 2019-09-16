7617ed970823a89e7ca14f3c2be0e1eb49902203
Hatem Mousselly Sergieh <hatem.mousselly-sergieh@telekom.de>
2019-09-16 09:32:20 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          changed here
+          <b>Hello form the feature branch :)</b>
         </p>
         <a
           className="App-link"
